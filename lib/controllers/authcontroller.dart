fdb8b50e263ee9cb865b35034888167605502b55
Your Name <you@example.com>
2022-11-14 19:37:21 +0500

    fire auth

--- lib/controllers/authcontroller.dart
+++ lib/controllers/authcontroller.dart
@@ -1,7 +1,12 @@
-import 'package:get/get.dart';
-class AuthController extends GetxController{
-trt(){
+import 'dart:io';
 
-}
-catch(){}
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+//create method for registering user
+  void reguser(String username, String email, String pass, File? image) {
+//try cath will listen to any errors
+    try {} catch (e) {}
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:tiktok/Auth/login.dart';
 import 'package:tiktok/Auth/signup.dart';
 import 'package:tiktok/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:tiktok/controllers/authcontroller.dart';
 
 void main() async {
   //so the framework knows that widgets have been initialized
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // this firebase will initalize the app for us
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
