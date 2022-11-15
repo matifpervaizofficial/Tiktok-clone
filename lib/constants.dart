a1b9b3db819d24c3ed71eefb33f92f8e94999f62
Your Name <you@example.com>
2022-11-15 19:19:20 +0500

    firebase config

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,8 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class MyColors {
@@ -6,4 +11,9 @@ class MyColors {
   static Color? ButtonColor = Colors.red[400];
 
   static const Color BorderColor = Colors.grey;
+
+  //firebase constants
+  var firebaseAuth = FirebaseAuth.instance;
+  var fireStorage = FirebaseStorage.instance;
+  var firestore = FirebaseFirestore.instance;
 }
--- lib/controllers/authcontroller.dart
+++ lib/controllers/authcontroller.dart
@@ -1,9 +1,31 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
 import 'dart:io';
 
-import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:tiktok/constants.dart';
+import 'package:tiktok/main.dart';
 
 class AuthController extends GetxController {
+  // upload image to firebase storage
+  Future<String> _uploadImagetoStorage(File image) async {
+    Reference ref = FirebaseStorage.instance
+        .ref()
+        .child("Profile")
+        .child(FirebaseAuth.instance.currentUser!.uid);
+    //upload image to firebase
+
+    UploadTask uploadTask = ref.putFile(image);
+    //take image from gallery and send to upload task
+    TaskSnapshot snap = await uploadTask;
+
+    String downloadurl = await snap.ref.getDownloadURL();
+    return downloadurl;
+  }
+
 //create method for registering user
   void reguser(String username, String email, String pass, File? image) async {
 //try cath will listen to any errors
@@ -11,9 +33,16 @@ class AuthController extends GetxController {
       if (username.isNotEmpty &&
           pass.isNotEmpty &&
           email.isNotEmpty &&
-          image != null)
-      //save user data in firebase
-      {}
+          image != null) {
+        //save user data in firebase
+
+        UserCredential cred =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email,
+          password: pass,
+        );
+        _uploadImagetoStorage(image);
+      }
     } catch (e) {
       Get.snackbar("Add Right Information", e.toString());
     }
